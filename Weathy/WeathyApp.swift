decd0c6197aceff5cf993c5072578a61ab4ac3ab
Mert Durkaya <mertdurkaya@gmail.com>
2023-11-26 19:36:56 +0100

    Initial Commit

---
+++ Weathy/WeathyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathyApp.swift
+//  Weathy
+//
+//  Created by Mert Durkaya on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeathyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
